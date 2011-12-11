ef57f1e83ed938b038d66c6adb1724936c9ddc1b
Rachman Chavik <rchavik@xintesa.com>
2011-12-11 08:21:50 +0700

    updating menu

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,3 +1,11 @@
 <?php
-	Croogo::hookAdminMenu('ClearCache');
 
+CroogoNav::add('extensions.children.clear_cache', array(
+	'title' => 'Clear Cache',
+	'url' => array(
+		'plugin' => 'clear_cache',
+		'controller' => 'clear_cache_clear_cache',
+		'action' => 'clear',
+		'admin' => true,
+		)
+	));
